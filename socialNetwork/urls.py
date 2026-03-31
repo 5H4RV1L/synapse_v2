a672cc0a0f45478377bddb89e073309eeea4fc45
5H4RV1L <156880539+5H4RV1L@users.noreply.github.com>
2026-04-01 02:05:46 +0530

    Add files via upload

---
+++ socialNetwork/urls.py
@@ -0,0 +1,60 @@
+"""
+URL configuration for socialNetwork project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('profile/', include('accounts.urls')),
+    path('chat/', include('chat.urls')),
+    path('friends/', include('friends.urls')),
+    path('notifications/', include('notifications.urls')),
+    path('', include('posts.urls')),
+    path('', include('accounts.urls')),
+
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT
+    }),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#if settings.DEBUG:
+#    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / 'static')
+#
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+from django.views.generic import RedirectView
+from django.templatetags.static import static
+
+urlpatterns += [
+    path("favicon.ico", RedirectView.as_view(url=static("favicon.ico")))
+]
